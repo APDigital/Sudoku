ab76515d4fd007d483b0bbf5b032ebd6c134e4ee
APDigital <aoifejo.pearson@googlemail.com>
2018-03-15 16:09:10 +0000

    Test Game

--- SudokuGame/Program.cs
+++ SudokuGame/Program.cs
@@ -10,6 +10,8 @@ namespace SudokuGame
     {
         static void Main(string[] args)
         {
+          
+
         }
     }
 }
---
+++ SudokuTest/TestGame.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SudokuGame;
+
+namespace SudokuTest
+{
+    [TestClass]
+    public class TestGame
+    {
+        [TestMethod]
+        public void TestGamePlay()
+        {
+            Board board = new Board();
+
+            board.SetSquareValue(1, 6, 2);
+            board.SetSquareValue(1, 7, 1);
+
+            board.SetSquareValue(2, 3, 4);
+            board.SetSquareValue(2, 6, 8);
+            board.SetSquareValue(2, 7, 7);
+
+            board.SetSquareValue(3, 2, 2);
+            board.SetSquareValue(3, 4, 3);
+            board.SetSquareValue(3, 7, 9);
+
+            board.SetSquareValue(4, 1, 6);
+            board.SetSquareValue(4, 3, 2);
+            board.SetSquareValue(4, 6, 3);
+            board.SetSquareValue(4, 8, 4);
+
+            board.SetSquareValue(6, 2, 5);
+            board.SetSquareValue(6, 4, 6);
+            board.SetSquareValue(6, 7, 3);
+            board.SetSquareValue(6, 9, 1);
+
+            board.SetSquareValue(7, 3, 3);
+            board.SetSquareValue(7, 6, 5);
+            board.SetSquareValue(7, 8, 8);
+
+            board.SetSquareValue(8, 3, 8);
+            board.SetSquareValue(8, 4, 2);
+            board.SetSquareValue(8, 7, 5);
+
+            board.SetSquareValue(9, 3, 9);
+            board.SetSquareValue(9, 4, 7);
+
+            var result =string.Empty;
+            foreach (var square in board.Squares)
+            {
+                result = string.Format("| {0}", square.Value);
+                Console.WriteLine(result);
+            }
+
+            Assert.AreNotEqual(null, result);
+        }
+    }
+}
